0644a146b8d683b70a7a86266864fc9bd5b79fa6
Alyxia Sother <alyxia@riseup.net>
2022-03-31 00:10:44 +0200

    [Global] Update deps and fix the bot

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import {Client} from "revolt.js";
-import {config} from "dotenv";
+import { Client } from 'revolt.js';
+import { config } from 'dotenv';
 
 const client = new Client();
 config();
 
-client.on("ready", () => {
-    console.info(`Logged in as ${client.user!.username}`);
+client.on('ready', () => {
+  console.info(`Logged in as ${client.user!.username}`);
 });
 
-client.on("message", (msg) => {
-    if (msg.content === "Hello") {
-        msg.channel!.sendMessage("Hi!");
-    }
+client.on('message', (msg) => {
+  if (msg.content === 'Hello') {
+    msg.channel!.sendMessage('Hi!');
+  }
 });
 
 client.loginBot(process.env.TOKEN!);
